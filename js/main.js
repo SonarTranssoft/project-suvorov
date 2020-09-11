feeb86f6b2548902948be88675c895bd73a2e7ad
SonarTransssoft <support@sonar55.ru>
2020-09-11 13:15:08 +0600

    Trying to make a clusters1

--- js/main.js
+++ js/main.js
@@ -5,13 +5,12 @@ class Place {
     }
 }
 
-// var locations = []
+// var locations1 = [{lat: 54.984951, lng: 73.4012343},
+// {lat: 54.9991464, lng: 73.3605812},
+// {lat: 55.0225655, lng: 73.31209559999999}
+// ]
 //Global array to set markers into a map
-// var locations = [
-//     {lat: 54.984951, lng: 73.4012343},
-//     {lat: 54.9991464, lng: 73.3605812},
-//     {lat: 55.0225655, lng: 73.31209559999999}
-// ];
+
 
 function getCoordinatesFromDeals() {
     let arr = [];
@@ -29,7 +28,7 @@ function getCoordinatesFromDeals() {
                     let dealIncompleteAddress = (el.UF_CRM_1598808869287);
                     let invalidCoordinates = dealIncompleteAddress.split('|');
                     let destination = invalidCoordinates[1].split(';');
-                    let place = new Place(parseFloat(destination[0]), parseFloat(destination[1]));
+                    let place = {lat: parseFloat(destination[0]), lng: parseFloat(destination[1])}
                     arr.push(place)
                 })
                 if (result.more())
@@ -47,12 +46,11 @@ function initMap() {
     // The map, centered at Uluru
     let map = new google.maps.Map(
         document.getElementById('map'), {zoom: 4, center: region});
-    let labels = 'ABC';
+    // let labels = 'ABC';
     console.log('Массив будущих маркеров', locations);
-    let markers = locations.map(function (location, i) {
-        return new google.maps.Marker({
+    let markers = locations.filter(function (location) {
+        new google.maps.Marker({
             position: location,
-            label: labels[i]
         });
     });
     console.log(markers);
@@ -61,6 +59,8 @@ function initMap() {
     let markerCluster = new MarkerClusterer(map, markers,
         {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
+var locations = [
+];
 
 function include(url) {
     let script = document.createElement('script');
